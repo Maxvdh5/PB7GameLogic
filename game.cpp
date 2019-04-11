260593354487a3f290b70cc97c7b548fbcf910ae
Maxvdh5 <maxvandenheijkant@gmail.com>
2019-04-11 12:18:37 +0200

    added documentation

--- game.cpp
+++ game.cpp
@@ -7,18 +7,7 @@
 #include "states.h"
 
 
-/*! \mainpage My Personal Index Page
- *
- * \section intro_sec Introduction
- *
- * This is the introduction.
- *
- * \section install_sec Installation
- *
- * \subsection step1 Step 1: Opening the box
- *
- * etc...
- */
+
 
 Game::Game()
 {
